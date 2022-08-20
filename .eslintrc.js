3f764ec650087e39b3ea8d3e2bca8a7a229e42ac
alecsandrapetruescu <alecsandra.petruescu@gmail.com>
2022-08-20 15:32:49 +0300

    Create .eslintrc.js

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+    root: true,
+    env: {
+      "node": true
+    },
+    extends: [
+      //"plugin:vue/essential",
+      'plugin:vue/vue3-recommended',
+      'eslint:recommended'
+    ],
+    rules: {},
+    parser: 'vue-eslint-parser',
+    parserOptions: {
+        parser: "@babel/eslint-parser",
+        sourceType: "module",
+        requireConfigFile: false,
+        babelOptions: {
+           babelrc: false,
+           configFile: false,
+          // your babel options
+          presets: ["@babel/preset-env"],
+        },
+   }	
+}
